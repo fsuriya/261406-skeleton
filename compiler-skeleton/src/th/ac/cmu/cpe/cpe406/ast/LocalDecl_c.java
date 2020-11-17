0017deeeeb3cfa87216357c73fd473ce642737c1
fsuriya <flukbanrong@gmail.com>
2020-11-18 00:23:56 +0700

    Update LocalDecl_c

--- compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/LocalDecl_c.java
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/LocalDecl_c.java
@@ -1,6 +1,11 @@
 package th.ac.cmu.cpe.cpe406.ast;
 
+import th.ac.cmu.cpe.cpe406.type.SymTable;
+import th.ac.cmu.cpe.cpe406.type.Type;
+import th.ac.cmu.cpe.cpe406.type.IntType_c;
+import th.ac.cmu.cpe.cpe406.type.BoolType_c;
 import th.ac.cmu.cpe.cpe406.util.Position;
+import th.ac.cmu.cpe.cpe406.type.UnitType_c;
 
 public class LocalDecl_c extends Stmt_c implements LocalDecl {
 
@@ -12,4 +17,29 @@ public class LocalDecl_c extends Stmt_c implements LocalDecl {
         this.id = id;
         this.typeNode = typeNode;
     }
+    
+    @Override
+    public Type TypeCheck(SymTable sym) throws Exception{
+    	Type typeToAdd = null;
+    	if (sym.lookup(id.name()) == null) {
+	    	if (typeNode.TypeCheck(sym).isInt()) {
+	    		typeToAdd = new IntType_c();
+	    	} else if (typeNode.TypeCheck(sym).isBool()){
+	    		typeToAdd = new BoolType_c();
+	    	} else {
+	    		throw new Exception("Compile error at " + pos.path() + "\nline:" + pos.line() + "\nError: type error");
+	    	}
+    	} else {
+    		String typeError = "Unknown Type";
+    		if (sym.lookup(id.name()).isInt()){
+    			typeError = "int";
+    		} else if (sym.lookup(id.name()).isBool()){
+    			typeError = "bool";
+    		} 
+    		throw new Exception("Compile error at " + pos.path() + "\nline:" + pos.line() + "\nError: Redeclaration of '" + id.name()  +":" + typeError + "'");
+    	}
+    	sym.add(id.name(), (Type)typeToAdd);
+    	
+		return new UnitType_c();
+	}
 }
--- compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/TypeNode.java
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/TypeNode.java
@@ -1,4 +1,9 @@
 package th.ac.cmu.cpe.cpe406.ast;
 
+import th.ac.cmu.cpe.cpe406.type.SymTable;
+import th.ac.cmu.cpe.cpe406.type.Type;
+
 public interface TypeNode extends Node {
+
+	Type TypeCheck(SymTable sym);
 }
