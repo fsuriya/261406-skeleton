747c7d1d7f427fdce5b2148968431acb086ce8e1
TouchySarun <25566sarun@gmail.com>
2020-11-18 01:59:15 +0700

    edit Typecheck for block add new symtable

--- compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Block_c.java
+++ compiler-skeleton/src/th/ac/cmu/cpe/cpe406/ast/Block_c.java
@@ -2,6 +2,7 @@ package th.ac.cmu.cpe.cpe406.ast;
 
 import java.util.List;
 import th.ac.cmu.cpe.cpe406.type.SymTable;
+import th.ac.cmu.cpe.cpe406.type.SymTable_c;
 import th.ac.cmu.cpe.cpe406.type.Type;
 import th.ac.cmu.cpe.cpe406.type.UnitType_c;
 import th.ac.cmu.cpe.cpe406.util.Position;
@@ -24,8 +25,9 @@ public class Block_c extends Expr_c implements Block {
 
 	@Override
 	public Type TypeCheck(SymTable sym) throws Exception {
+		SymTable newsym = new SymTable_c(sym);
 		for(Stmt x: statements) {
-			Type xType = ((Stmt_c) x).TypeCheck(sym);
+			Type xType = ((Stmt_c) x).TypeCheck(newsym);
 			if(xType != null) {
 				if(!xType.isUnit()) {
 					throw new Exception("Type error at: "+ pos.path() + " line: "+ pos.line() + "\n Error: This Stmt is sus!!!");
